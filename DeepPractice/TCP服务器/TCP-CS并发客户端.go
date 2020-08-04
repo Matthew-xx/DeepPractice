11c8d8e902074d4e407313ba3bc638f2559fe975
Matthew-xx <1659530762@qq.com>
2020-08-04 13:41:39 +0800

    TCP服务器

---
+++ DeepPractice/TCP服务器/TCP-CS并发客户端.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main()  {
+	//主动发起连接请求
+	conn,err :=net.Dial("tcp","127.0.0.1:8001")
+	if err != nil {
+		fmt.Println("Dial err:",err)
+		return
+	}
+	defer conn.Close()
+
+	//获取用户输入，将输入数据发送给服务器
+	go func() {
+		str := make([]byte,4096)
+		for  {
+			n,err :=os.Stdin.Read(str)
+			if err != nil{
+				fmt.Println("os read err:",err)
+				continue
+			}
+			//写给服务器
+			conn.Write(str[:n])
+		}
+	}()
+
+	//显示服务器回发的内容
+	buf := make([]byte,4096)
+	for {
+		n,err := conn.Read(buf)
+		if n == 0 {
+			fmt.Println("退出连接")
+			return
+		}
+		if err != nil {
+			fmt.Println("conn.read err:",err)
+			return
+		}
+		fmt.Println("服务器回发：",string(buf[:n]))
+	}
+}
---
+++ DeepPractice/TCP服务器/TCP-CS并发服务器.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+func main()  {
+	//创建监听套接字
+	listener,err := net.Listen("tcp","127.0.0.1:8001")
+	if err!= nil {
+		fmt.Println("net.listener err:",err)
+		return
+	}
+	defer listener.Close()
+
+	for  {
+		fmt.Println("服务器等待连接...")
+		//监听客户端连接请求(循环监听多个客户端
+		conn,err := listener.Accept()
+		if err!= nil {
+			fmt.Println("Accept err:",err)
+			return
+		}
+
+		//具体完成服务器和客户端的数据通信(go程，连接一个客户端开一个go程去处理，
+		// 而不用全部直接去请求服务器，如果是全部直接去请求服务器，那么需要排队等待处理
+		go HandlerConnect(conn)
+	}
+
+}
+
+func HandlerConnect(conn net.Conn)  {
+	defer conn.Close()
+	
+	addr := conn.RemoteAddr()
+	fmt.Println(addr,"客户端成功连接")
+	//循环读取客户端发送的数据
+	buf := make([]byte,4096)
+	for  {
+		//for循环避免一次性操作,直到客户端关闭
+		n,err := conn.Read(buf)
+		if string(buf[:n]) == "exit\r\n" || string(buf[:n]) == "exit\n"{
+			fmt.Println("客户端退出")
+			return
+		}
+		if n == 0 {
+			fmt.Println("客户端已断开")
+			return
+		}
+		if err != nil {
+			fmt.Println("Read err:",err)
+			return
+		}
+		fmt.Println("服务器读取数据：",string(buf[:n])) //使用数据
+		//处理数据,回发给客户端
+		_,err = conn.Write([]byte(strings.ToUpper(string(buf[:n]))))
+		if err!= nil {
+			fmt.Println("Write err:",err)
+			return
+		}
+	}
+}
